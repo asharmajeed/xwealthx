d62d612c2754a62e95e77a3a54d73b7e594d3326
asharmajeed <asharmajeed40@gmail.com>
2024-11-14 23:23:40 +0500

    update cors

--- client/src/api.js
+++ client/src/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const api = axios.create({
     baseURL: "https://xwealthx.vercel.app/auth/",
-    withCredentials: true,
+    // withCredentials: true,
 });
 
 export const googleAuth = (code) => api.get(`/google?code=${code}`);
