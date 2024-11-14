eadf85a7d1177c59061451198813b5f09d4c241e
asharmajeed <asharmajeed40@gmail.com>
2024-11-14 23:04:21 +0500

    change local url to vercel url

--- client/src/api.js
+++ client/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: "http://localhost:8080/auth/",
+    baseURL: "https://xwealthx.vercel.app/auth/",
     // withCredentials: true,
 });
 
