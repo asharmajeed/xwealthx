be6e0fc517c9b51098663d74b59fa51729c71408
asharmajeed <asharmajeed40@gmail.com>
2024-11-14 22:58:51 +0500

    udpate index.js

--- backend/index.js
+++ backend/index.js
@@ -9,10 +9,6 @@ const PORT = process.env.PORT || 8080;
 app.use(cors());
 app.use('/auth/', authRoutes); // <- NEW LINE
 
-app.all('*', (req, res, next) => {
-    next(new AppError(`Can't find ${req.originalUrl} on the server`, 404));
-});
-
 app.listen(PORT, () => {
     console.log(`Server is running on ${PORT}`)
 })
