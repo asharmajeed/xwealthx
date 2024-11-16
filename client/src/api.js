f0f68d509201270ea2540aa26b8522259112138a
asharmajeed <asharmajeed40@gmail.com>
2024-11-16 18:03:13 +0500

    update api.js

--- client/src/api.js
+++ client/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: "https://xwealthx.vercel.app/auth/",
+    baseURL: "https://xwealthx.vercel.app/api/auth/",
     // withCredentials: true,
 });
 
