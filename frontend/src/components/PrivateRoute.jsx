6892d04eb369be92f02b96cb22e9e90e2510a73b
asharmajeed <asharmajeed40@gmail.com>
2024-11-19 23:17:53 +0500

    update private route

--- frontend/src/components/PrivateRoute.jsx
+++ frontend/src/components/PrivateRoute.jsx
@@ -4,7 +4,7 @@ import { useUser } from "../context/UserContext";
 const PrivateRoute = () => {
   const { userInfo } = useUser();
 
-  return userInfo ? <Outlet /> : <Navigate to="/login" replace />;
+  return userInfo ? <Outlet /> : <Navigate to="/" replace />;
 };
 
 export default PrivateRoute;
