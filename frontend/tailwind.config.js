99f2f99c2b90f8a6597fc51f926dd44c37f18b83
asharmajeed <asharmajeed40@gmail.com>
2024-11-23 12:19:30 +0500

    update tailwind config

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,26 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
       colors: {
-        pastelGreen: '#9dcd5a',
-        pink: '#e61f93',
-        pastelPink: '#ffacec',
-        grey: '#7d7d7d',
+        pastelGreen: "#9dcd5a",
+        // pink: '#e61f93',
+        pastelPink: "#ffacec",
+        grey: "#7d7d7d",
         teal: {
-          DEFAULT: '#1b7895',
-          dark: '#0a3c4b',   // Dark teal
-          light: '#4ab5c7',   // Light teal, optional if needed
+          DEFAULT: "#1b7895",
+          dark: "#0a3c4b", // Dark teal
+          light: "#4ab5c7", // Light teal, optional if needed
         },
         // darkTeal: '#0a3c4b',
-        blue: '#005b99',
-        backgroundBlue: '#a2c8e1'
-      }
-    }
+        // blue: '#005b99',
+        backgroundBlue: "#a2c8e1",
+      },
+    },
   },
   plugins: [],
-}
+};
