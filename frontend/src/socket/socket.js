9dd0f027158182b2464e677f53376608aae476fe
asharmajeed <asharmajeed40@gmail.com>
2024-12-04 16:41:33 +0500

    update socket.js frontend

--- frontend/src/socket/socket.js
+++ frontend/src/socket/socket.js
@@ -1,7 +1,7 @@
 import { io } from "socket.io-client";
 import { BASE_URL } from "../utils/constants";
 
-const socket = io(BASE_URL); // Backend WebSocket server
+const socket = io(BASE_URL, { withCredentials: true }); // Backend WebSocket server
 
 export const registerUser = (userId) => {
   socket.emit("registerUser", userId); // Register the user with their userId
