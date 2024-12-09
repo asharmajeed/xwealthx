717a25d5cc6d7e8b32c1b3568a2d5d2719251fdf
asharmajeed <asharmajeed40@gmail.com>
2024-12-09 12:22:07 +0500

    Add 2nd frontend

--- backend/index.js
+++ backend/index.js
@@ -14,7 +14,7 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: "https://xwealthx-8dga.vercel.app",
+    origin: ["https://xwealthx-8dga.vercel.app", "https://xwealthx-2ayz.vercel.app"],
     credentials: true,
   })
 );
